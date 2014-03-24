d252bf6c1777bafded5410f52937c8b49dc4079d
Dariusz Kobylarz <darek.kobylarz@gmail.com>
2014-03-24 17:42:36 +0100

    twenty news example updates

--- twentynewsclassificationapp/src/ai/vital/twentynewsClassificationApp/TwentyNewsClassificationApp.groovy
+++ twentynewsclassificationapp/src/ai/vital/twentynewsClassificationApp/TwentyNewsClassificationApp.groovy
@@ -1,5 +1,8 @@
 package ai.vital.twentynewsClassificationApp
 
+import ai.vital.predict.PredictModel
+import ai.vital.vitalsigns.VitalSigns;
+
 import org.example.twentynews.*
 
 
@@ -8,33 +11,44 @@ class TwentyNewsClassificationApp {
 	static main(args) {
 	
 		
-		
 		// Input properties
 		
 		String subject = "Let's play softball in the park!"
 		
 		String body = "Softball game tonight.  Bring your bats!"
 		
+		// Model Jar Path
+		 
+		String modelJarPath = "..."
+		
+		
+		// Initialize Predict Model
+		PredictModel model = new PredictModel()
+		
+		model.loadJar(new File(modelJarPath))
+		
+		// Do prediction
+		// predict.predict(mydoc)
+
 		// Create instance of TwentyNewsDocument
 		
-		def mydoc = new TwentyNewsDocument()
+		TwentyNewsDocument mydoc = new TwentyNewsDocument()
 		
 		mydoc.URI = "http://example.org/twentynews/TwentyNewsDocument/123"
 		mydoc.title = subject
 		mydoc.body = body
+
 		
-		// Model Jar Path 
-		
-		
-		// Initialize Predict Model
-		
-		
-		// Do prediction
-		// predict.predict(mydoc)
+				
+		model.predict(mydoc)
 		
 		
 		// Display categories with scores in edges
-		// mydoc.getCategories()
+		List<Category> categories = mydoc.getNewsCategories()
+		
+		for( int i = 0 ; i < categories.size(); i++ ) {
+			println categories[i].name
+		}
 		
 		
 		
