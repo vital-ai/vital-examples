b67f285e8b95a32e3c5a8a60f88a84cc28dca55e
hadfield <marc@vital.ai>
2015-04-13 20:13:32 -0400

    vitalservice sample

--- vital-samples/src/main/groovy/ai/vital/samples/SampleManagedOntology.groovy
+++ vital-samples/src/main/groovy/ai/vital/samples/SampleManagedOntology.groovy
@@ -8,12 +8,14 @@ import vitalai.samples.domain.*
 import ai.vital.vitalsigns.VitalSigns
 import ai.vital.vitalsigns.model.*
 
-
+import vitalai.samples.domain.ontology.Ontology
 
 class SampleManagedOntology {
 
-	static App app
 	
+	// This is the name of the App, and the name of the Organization
+	// This is used to help generate URIs
+	static App app
 	static {
 		app = new App(ID: 'app', organizationID: 'organization')
 	}
@@ -22,14 +24,23 @@ class SampleManagedOntology {
 	
 		VitalSigns vs = VitalSigns.get()
 		
+		// This helps VitalSigns generate URIs
 		vs.setCurrentApp(app)
 		
-		String base_uri = "http://vital.ai/ontology/vital-samples"
 		
+		// This is the ontology class defined in our domain:
+		// vitalai.samples		
+		String base_uri = Ontology.ONTOLOGY_IRI
+		
+		// A list to hold our classes, we'll add this into the in-memory cache
 		def myobjs = []
 		
 		println "Hello World!"
 		
+		// Generate a URI with "john" as the unique part.  We know this is unique for this app, 
+		// but in generate we would allow VitalSigns to generate a randomized URI, that we can be sure is
+		// globally unique
+		 
 		Musician john = new Musician().generateURI("john")
 		
 		john.name = "John Lennon"
@@ -58,6 +69,7 @@ class SampleManagedOntology {
 		
 		thebeatles.name = "The Beatles"
 		
+		// try to assign the wrong property, throws an exception
 		try { 
 		thebeatles.birthday = "January 1, 1970"^xsd.xdatetime("MMMM d, yyyy")
 		} catch(Exception ex) { println ex } // no such property exception
@@ -65,56 +77,61 @@ class SampleManagedOntology {
 		
 		myobjs.addAll([john, paul, george, ringo, thebeatles])
 		
+		// Link the The Beatles to its members
 		myobjs.add( thebeatles.addEdge_hasMember(john) )
 		myobjs.add( thebeatles.addEdge_hasMember(paul) )
 		myobjs.add( thebeatles.addEdge_hasMember(george) )
 		myobjs.add( thebeatles.addEdge_hasMember(ringo) )
 		
-		//myobjs.add( new Edge_hasMember().generateURI().addSource(thebeatles).addDestination(john) )
-		//myobjs.add( new Edge_hasMember().generateURI().addSource(thebeatles).addDestination(paul) )
-		//myobjs.add( new Edge_hasMember().generateURI().addSource(thebeatles).addDestination(george) )
-		//myobjs.add( new Edge_hasMember().generateURI().addSource(thebeatles).addDestination(ringo) )
 		
-		
+		// We could create a new instance for an intrument
 		//MusicInstrument guitar = new MusicInstrument().generateURI("guitar")
-		
 		//guitar.name = "Guitar"
-		
 		//myobjs.add( guitar )
 		
-		// get individual
-		
+		// but let's get an individual defined in our domain ontology
+		// we happen to know its URI...
 		def g_uri = URIProperty.withString(base_uri + "#" + "Guitar")
 		
 		def guitar  = vs.getIndividual(g_uri.toString())
 		
 		myobjs.add(guitar)
 		
-		//println "Guitar Individual: " + guitar
 		
+		// We could have listed all the music instruments in our domain ontology...
 		//vs.listDomainIndividuals(MusicInstrument.class, null).each { println "Individual: " + it }
 		
-		myobjs.add( new Edge_playsInstrument().generateURI().addSource(john).addDestination(guitar) )
+		// Let's add some "plays" links...
 		
-		myobjs.add( new Edge_playsInstrument().generateURI().addSource(george).addDestination(guitar) )
+		myobjs.add( john.addEdge_playsInstrument(guitar) )
+		myobjs.add( george.addEdge_playsInstrument(guitar) )
 		
-		//myobjs.add( paul.addEdge_playsInstrument(guitar) )
-		
+		// here's how comparisons can be mad...
 		println "Is John's birthday before Paul's? : " + ( john.birthday < paul.birthday)
 		
-		vs.addToCache(myobjs)
 		
+		// Let's use different serialization methods to print out our objects
 		myobjs.each {
 			
 			println it.toRDF()
+			println it.toJSON()
+			println it.toCompactString()
 			
 		}
 		
+		// Let's put all those objects into the cache
+		// This will allow our helper methods to resolve links
+		vs.addToCache(myobjs)
+		
+		// Follow the hasMember edges...
 		println "The Beatles members: "
 		thebeatles.getMembers().each{ println it.name } 
 		
+		// Follow the plays edges in reverse: Instrument <-- Musician
 		println "Who plays guitar: "
 		guitar.getPlaysInstrumentsReverse().each{ println it.name }
+		
+		// Follow the plays edges in forward directions: Musician --> Instrument
 		
 		println "John plays what: " 
 		john.getPlaysInstruments().each { println it.name }
--- vital-samples/src/main/groovy/ai/vital/samples/SampleVitalService.groovy
+++ vital-samples/src/main/groovy/ai/vital/samples/SampleVitalService.groovy
@@ -1,8 +1,143 @@
 package ai.vital.samples
 
-class SampleVitalService {
+import ai.vital.query.graphbuilder.GraphQueryBuilder
+import ai.vital.vitalservice.VitalService
+import ai.vital.vitalservice.factory.Factory
+import ai.vital.vitalservice.model.App
+import ai.vital.vitalservice.query.ResultList
+import ai.vital.vitalservice.query.graph.VitalGraphQuery
+import ai.vital.vitalservice.query.graph.VitalSelectQuery
+import ai.vital.vitalsigns.VitalSigns
+import ai.vital.query.graph.*
 
+import ai.vital.vitalsigns.model.*
+import ai.vital.vitalsigns.model.properties.*
+import static ai.vital.query.graph.Utils.*
+
+import ai.vital.domain.*
+import ai.vital.domain.properties.*
+
+import ai.vital.vitalservice.segment.VitalSegment
+
+
+class SampleVitalService {
+	
 	static main(args) {
+	
+		VitalSigns vs = VitalSigns.get()
+		
+		VitalService service = Factory.getVitalService()
+		
+		println "Ping: " + service.ping()
+		
+		println "Endpoint: " + service.getEndpointType()
+		
+		println "Organization: " + service.getOrganization()
+		
+		println "App: " + service.getApp().ID
+		
+		service.listSegments().each{ println "Segment: " + it.id }
+		
+		println "Default Segment Name: " + service.getDefaultSegmentName()
+		
+		println "Validate: " + service.validate()
+		
+		def builder = new GraphQueryBuilder()
+		
+		// Select Adjectives with "happy" in the name (case indifferent)
+		
+		VitalSelectQuery q = builder.query {
+			
+			SELECT {
+				
+				value segments: [VitalSegment.withId('wordnet')]
+				
+				value limit: 100
+				
+				node_constraint { AdjectiveSynsetNode.class }
+				
+				node_constraint { AdjectiveSynsetNode.props().name.contains_i("happy") }
+			}
+			
+		}.toQuery()
+		
+		println "Sparql:\n" + q.toSparql()
+		
+		ResultList list = service.query( q )
+		
+		println "Results:\n" + list.results.size()
+		
+		// print out: URI : name
+		
+		def i = 1
+		
+		list.each { println i++ + ": " + it.URI + " : " + it.name }
+		
+		// Find the graph of all words with "happy" in its name and what it's connected to (one edge hop)
+		
+		
+		VitalGraphQuery q2 = builder.query {
+			
+			GRAPH {
+				
+				value segments: [VitalSegment.withId('wordnet')]
+				
+				value inlineObjects: true
+				
+				ARC {
+					
+					// bind this node to name "node1"
+					node_bind { "node1" }
+					
+					// include subclasses of SynsetNode: Noun, Verb, Adjective, Adverb
+					node_constraint { SynsetNode.expandSubclasses(true) }
+							
+					node_constraint { SynsetNode.props().name.contains_i("happy") }
+		
+					// alternate way to specify constraint
+					//node_constraint { PropertyConstraint(Property_hasName.class).contains_i("happy") }
+								
+					ARC {
+						// bind the node and edge to names "node2" and "edge"
+						edge_bind { "edge" } 
+						node_bind { "node2" } 
+						}	
+				}
+			}
+	
+		}.toQuery()
+			
+		println "Sparql:\n" + q2.toSparql()
+		
+		ResultList list2 = service.query( q2 )
+		
+		println "Results:\n" + list2.results.size()
+		
+		def j = 1
+		
+		list2.each { 
+			
+			// Use the binding names to get the URI values out of GraphMatch
+			def node1_uri = it."node1".toString()
+			def edge_uri = it."edge".toString()
+			def node2_uri = it."node2".toString()
+			
+			// inlineObjects is true, which embeds unseen objects into the results
+			// if cache is null, get graph object out of GraphMatch results
+			// graph objects referenced via the URI 
+						
+			def node1 = vs.getFromCache(node1_uri) ?: it."$node1_uri"
+			def edge = vs.getFromCache(edge_uri) ?: it."$edge_uri"
+			def node2 = vs.getFromCache(node2_uri) ?: it."$node2_uri"
+			
+			// add new ones into cache, doesn't hurt to refresh existing ones
+			vs.addToCache([node1, edge, node2])
+				
+			// print out node1 --edge--> node2, with edge type (minus the namespace)
+			println j++ + ": " + node1.name + "---" + (edge.vitaltype.toString() - "http://vital.ai/ontology/vital#") + "-->" + node2.name
+		}
+		
+		service.close()
 	
 	}
 
