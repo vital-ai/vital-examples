43eee1df66e710f5ccb82e3c121f08bcb0418b9d
Dariusz Kobylarz <darek.kobylarz@gmail.com>
2015-11-19 21:51:46 +0100

    language model builder with PREDICT

--- language-detect/builder/language-detector-model.dsl.builder.groovy
+++ language-detect/builder/language-detector-model.dsl.builder.groovy
@@ -3,6 +3,7 @@ import ai.vital.vitalsigns.block.BlockCompactStringSerializer.VitalBlock
 import ai.vital.vitalsigns.model.GraphObject
 import ai.vital.aspen.nlp.LanguageDetectorFactory;
 import ai.vital.aspen.model.BuilderFunctionPrediction;
+import ai.vital.aspen.model.CategoryPrediction;
 import ai.vital.aspen.groovy.nlp.steps.LanguageDetectorStep;
 
 import com.vitalai.domain.nlp.Annotation
@@ -49,18 +50,15 @@ MODEL {
 		}
 		
 	}
-
-
-	TARGET {
-
-		value function: { VitalBlock block, Map features, Prediction result ->
-
-			BuilderFunctionPrediction bfp = result
+	
+	PREDICT {
+		
+		value function: { VitalBlock block,  Map features, Map modelConfig ->
 			
 			def doc = block.getMainObject()
-
+			
 			LanguageDetectorStep languageDetector = LanguageDetectorFactory.getInstance()
-
+			
 			String lang = null
 			
 			for(GraphObject g : languageDetector.processDocument(doc)) {
@@ -68,6 +66,24 @@ MODEL {
 					lang = ((Annotation)g).annotationValue?.toString()
 				}
 			}
+			
+			BuilderFunctionPrediction prediction = new BuilderFunctionPrediction()
+			prediction.value = lang
+			return prediction
+			
+		}
+	}
+
+
+	TARGET {
+
+		value function: { VitalBlock block, Map features, Prediction result ->
+
+			def doc = block.getMainObject()
+			
+			BuilderFunctionPrediction bfp = result
+			
+			String lang = bfp.value
 			
 			doc.lang = lang
 			
