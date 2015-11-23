a806ba130c3c8ebdc413a6a93ed3c70ebf225cd9
Derek Kobylarz <darek.kobylarz@gmail.com>
2015-11-23 21:12:25 +0100

    SPARQL and SQL queries examples

--- vital-samples/src/main/groovy/ai/vital/samples/SampleSPARQLNamedDatabase.groovy
+++ vital-samples/src/main/groovy/ai/vital/samples/SampleSPARQLNamedDatabase.groovy
@@ -1,9 +1,167 @@
 package ai.vital.samples
 
+import ai.vital.query.querybuilder.VitalBuilder
+import ai.vital.vitalservice.VitalStatus;
+import ai.vital.vitalservice.factory.VitalServiceFactory
+import ai.vital.vitalservice.query.ResultList;
+import ai.vital.vitalservice.query.VitalExternalSparqlQuery;
+import ai.vital.vitalsigns.model.DatabaseConnection;
+import ai.vital.vitalsigns.model.GraphObject
+import ai.vital.vitalsigns.model.RDFStatement
+import ai.vital.vitalsigns.model.SparqlAskResponse;
+import ai.vital.vitalsigns.model.SparqlBinding;
+import ai.vital.vitalsigns.model.SparqlDatabaseConnection;
+import ai.vital.vitalsigns.model.VitalApp
+import ai.vital.vitalsigns.model.VitalServiceAdminKey
+
+
+/**
+ * Sample named SPARQL Database query example
+ * Requirements:
+ * - an allegrograph database instance with wordnet data loaded: 
+ *   https://github.com/vital-ai/vital-datasets/raw/master/Wordnet/wordnet.0.2.300.nt.gz
+ * - a 'default' profile configured as LuceneMemory endpoint
+ * - updated sparqlDBConnection object properties (below)
+ *
+ * @author Derek
+ *
+ */
 class SampleSPARQLNamedDatabase {
+	
+	static String databaseName = 'agraph1'
 
 	static main(args) {
 	
+		SparqlDatabaseConnection sparqlDBConnection = new SparqlDatabaseConnection()
+		sparqlDBConnection.catalogName = '' //usually empty, but fill it in if custom agraph catalog
+		sparqlDBConnection.endpointType = 'Allegrograph'
+		sparqlDBConnection.endpointURL = 'http://example.org:10035'
+		sparqlDBConnection.name = databaseName
+		sparqlDBConnection.password = 'password'
+		sparqlDBConnection.repositoryName = 'repositoryName'
+		sparqlDBConnection.username = 'username'
+		
+		
+		//use default inmemory profile
+		VitalServiceAdminKey key = new VitalServiceAdminKey().generateURI((VitalApp) null)
+		key.key = 'aaaa-aaaa-aaaa'
+		
+		def adminService = VitalServiceFactory.openAdminService(key)
+		VitalApp app = VitalApp.withId('app') 
+		adminService.addApp(app)
+		
+		
+		VitalStatus addStatus = adminService.addDatabaseConnection(app, sparqlDBConnection)
+		
+		println "ADD DB connection status: ${addStatus}"
+		
+		for(GraphObject g : adminService.listDatabaseConnections(app)) {
+			DatabaseConnection dbConn = g
+			println "Connection: ${dbConn.name} - ${dbConn.endpointType} - ${dbConn.endpointURL}"
+		}
+	
+		
+		//sample SELECT QUERY
+		
+        VitalExternalSparqlQuery selectQuery = new VitalBuilder().query {
+            
+            SPARQL {
+                
+                value database: databaseName
+                
+                value sparql: """\
+
+SELECT ?s ?p ?o WHERE {
+  ?s ?p ?o .
+  ?s a <http://vital.ai/ontology/vital-wordnet#NounSynsetNode> .
+  ?s <http://vital.ai/ontology/vital-core#hasName> "thing" .
+}
+
+""" 
+				
+                
+            }
+            
+        }.toQuery()
+		
+			
+		ResultList selectRL = adminService.query(app, selectQuery)
+		println "SELECT Query status: ${selectRL.status}"
+		
+		for(GraphObject g : selectRL) {
+			
+			SparqlBinding binding = g
+			
+			println "${binding.s} ${binding.p} ${binding.o}"
+			
+		}
+		
+		
+		//SAMPLE CONSTRUCT QUERY
+		VitalExternalSparqlQuery constructQuery = new VitalBuilder().query {
+			
+			SPARQL {
+				
+				value database: databaseName
+				
+				value sparql: """\
+
+CONSTRUCT { ?s ?p ?o } WHERE {
+  ?s ?p ?o .
+  ?s a <http://vital.ai/ontology/vital-wordnet#NounSynsetNode> .
+  ?s <http://vital.ai/ontology/vital-core#hasName> "thing" .
+}
+
+""" 
+				
+				
+			}
+			
+		}.toQuery()
+		
+			
+		ResultList constructRL = adminService.query(app, constructQuery)
+		println "CONSTRUCT Query status: ${constructRL.status}"
+		
+		for(GraphObject g : constructRL) {
+			
+			RDFStatement binding = g
+			
+			println "${binding.rdfSubject} ${binding.rdfPredicate} ${binding.rdfObject}"
+			
+		}
+
+		
+		//SAMPLE ASK QUERY
+		VitalExternalSparqlQuery askQuery = new VitalBuilder().query {
+			
+			SPARQL {
+				
+				value database: databaseName
+				
+				value sparql: """\
+
+ASK  { ?s <http://vital.ai/ontology/vital-core#hasName> "thing" . }
+
+"""
+				
+			}
+			
+		}.toQuery()
+		
+		ResultList askRL = adminService.query(app, askQuery)
+		println "ASK Query status: ${askRL.status}"
+		
+		for(GraphObject g : askRL) {
+			
+			SparqlAskResponse askRes = g
+			
+			println "ASK response: ${askRes.positiveResponse}"
+			
+		}
+		
+			
+			
 	}
 
 }
--- vital-samples/src/main/groovy/ai/vital/samples/SampleSQLNamedDatabase.groovy
+++ vital-samples/src/main/groovy/ai/vital/samples/SampleSQLNamedDatabase.groovy
@@ -1,9 +1,90 @@
 package ai.vital.samples
 
+import ai.vital.query.querybuilder.VitalBuilder
+import ai.vital.vitalservice.VitalStatus
+import ai.vital.vitalservice.factory.VitalServiceFactory
+import ai.vital.vitalservice.query.ResultList
+import ai.vital.vitalservice.query.VitalExternalSqlQuery;
+import ai.vital.vitalsigns.model.DatabaseConnection
+import ai.vital.vitalsigns.model.GraphObject
+import ai.vital.vitalsigns.model.SqlDatabaseConnection
+import ai.vital.vitalsigns.model.SqlResultRow;
+import ai.vital.vitalsigns.model.VitalApp
+import ai.vital.vitalsigns.model.VitalServiceAdminKey
+import ai.vital.vitalsigns.model.property.IProperty;
+import java.util.Map.Entry
+
+/**
+ * Sample named SQL Database query example
+ * Requirements:
+ * - a mysql database instance with data loaded from https://github.com/datacharmer/test_db
+ * - a 'default' profile configured as LuceneMemory endpoint
+ * - updated sqlDBConnection object properties (below)
+ * 
+ * @author Derek
+ *
+ */
 class SampleSQLNamedDatabase {
 
-	static main(args) {
+	static String databaseName = 'localmysql1'
 	
+	static main(args) {
+
+		SqlDatabaseConnection sqlDBConnection = new SqlDatabaseConnection()
+		sqlDBConnection.database = 'employees'
+		sqlDBConnection.endpointType = 'MySQL'
+		sqlDBConnection.endpointURL = 'jdbc:mysql://localhost:3306'
+		sqlDBConnection.name = databaseName
+		sqlDBConnection.password = 'password'
+		sqlDBConnection.username = 'username'
+		
+			
+		//use default inmemory profile
+		VitalServiceAdminKey key = new VitalServiceAdminKey().generateURI((VitalApp) null)
+		key.key = 'aaaa-aaaa-aaaa'
+		
+		def adminService = VitalServiceFactory.openAdminService(key)
+		VitalApp app = VitalApp.withId('app')
+		adminService.addApp(app)
+		
+		VitalStatus addStatus = adminService.addDatabaseConnection(app, sqlDBConnection)
+		
+		println "ADD DB connection status: ${addStatus}"
+		
+		for(GraphObject g : adminService.listDatabaseConnections(app)) {
+			DatabaseConnection dbConn = g
+			println "Connection: ${dbConn.name} - ${dbConn.endpointType} - ${dbConn.endpointURL}"
+		}
+	
+		VitalExternalSqlQuery query = new VitalBuilder().query {
+			
+			SQL {
+				
+				value database: sqlDBConnection.name.toString()
+				
+				value sql: """\
+
+ select * from employees limit 10
+
+""" 
+				
+				
+			}
+			
+		}.toQuery()
+		
+			
+		ResultList rl = adminService.query(app, query)
+		println "Query status: ${rl.status}"
+		
+		for(GraphObject g : rl) {
+			
+			SqlResultRow row = g
+
+			println row			
+			
+		}
+		
 	}
 
 }
